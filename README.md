6a320c82f28471a24533097b660f70a2b4484e20
alexanderkif <alexander_kif@mail.ru>
2020-03-13 22:12:07 +0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# dometeo
+# DoMeteo (dometeo)
+
 Repeat this project for free. Use Node JS as a backend, Quasar (Vue) as a frontend.
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/ToBuy.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page class="full-width" style="max-width: 1280px;">
+    <h5 class="text-center">Если спаять пару датчиков и немного запрограммировать,</h5>
+    <div class="full-width flex flex-center q-px-md">
+      <q-img
+        class="rounded-borders"
+        src="../statics/schema-min.png"
+        spinner-color="secondary"
+        style="max-width: 800px"
+      ></q-img>
+    </div>
+    <h5 class="text-center">то можно записать показания этих датчиков в базу данных
+      для анализа,</h5>
+    <div class="full-width flex flex-center">
+      <q-img
+        src="../statics/desktop-tv.png"
+        spinner-color="secondary"
+        style="max-width: 800px"
+      ></q-img>
+    </div>
+    <h5 class="text-center">ну или просто наблюдать и делать прогнозы</h5>
+    <div class="flex flex-center row justify-center">
+      <q-img
+        class="col-7"
+        src="../statics/mobile-hor-menu-ip.png"
+        spinner-color="secondary"
+        style="max-width: 500px"
+      ></q-img>
+      <q-img
+        class="col-5 q-ml-lg"
+        src="../statics/mobile-vert-lg.png"
+        spinner-color="secondary"
+        style="max-width: 300px"
+      ></q-img>
+    </div>
+  </q-page>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+
+export default {
+  name: 'ToBuy',
+  components: {},
+  data () {
+    return {
+      visible: false
+    }
+  },
+  created () {
+    this.visible = true
+  },
+  methods: {}
+}
+</script>
