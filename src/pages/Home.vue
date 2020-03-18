a189688ee99ff4bdf59e89f3952129ea37951879
Никифоров Александр Валерьевич <AlVaNikiforov@GNGLZ.LOCAL>
2020-03-18 15:38:15 +0400

    Added HowItWorks

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <q-page class="flex flex-center full-width q-pa-md" style="max-width: 1280px;">
+  <q-page class="flex flex-center full-width q-pa-md text-subtitle1 text-justify" style="max-width: 1280px;">
     <div style="max-width: 1024px">
       <div class="full-width flex flex-center q-pa-md">
         <q-img
           class="rounded-borders"
-          src="../statics/how.png"
+          src="../statics/how.svg"
           spinner-color="secondary"
           style="max-width: 631px"
           contain
         ></q-img>
       </div>
-      <div class="text-justify text-subtitle1 q-pa-sm">Хотите контролировать датчики метеоданных или света, открытие дверей или окон,
+      <div class="q-pa-sm">Хотите контролировать датчики метеоданных или света, открытие дверей или окон,
             включение электроприборов или уровень углекислого газа в течение ночи? Хотелось
             бы иметь графики этих событий для контроля или анализа за определенный период?</div>
       <div class="flex flex-center row justify-center">
@@ -27,9 +27,9 @@
           style="max-width: 385px"
         ></q-img>
       </div>
-      <div class="text-justify text-subtitle1 q-pa-sm">А может собираетесь построить зависимость температуры в помещениях вашего
+      <div class="q-pa-sm">А может собираетесь построить зависимость температуры в помещениях вашего
             энергосберегающего умного дома от направления ветра и температуры снаружи?</div>
-      <div class="text-justify text-subtitle1 q-pa-sm">Или хотите узнать, закрытие какой из межкомнатных дверей останавливает конвекцию
+      <div class="q-pa-sm">Или хотите узнать, закрытие какой из межкомнатных дверей останавливает конвекцию
             воздуха внутри дома и уменьшает затраты на отопление?</div>
       <div class="full-width flex flex-center q-pa-md">
         <q-img
@@ -38,9 +38,9 @@
           style="max-width: 801px;"
         ></q-img>
       </div>
-      <div class="text-justify text-subtitle1 q-pa-sm">Или вы хотите легко и играючи познакомить себя или вашего ребенка с современным
+      <div class="q-pa-sm">Или вы хотите легко и играючи познакомить себя или вашего ребенка с современным
             веб-программированием и микроконтроллерами ардуино?</div>
-      <div class="text-justify text-subtitle1 q-pa-sm">Если спаять пару датчиков и немного их запрограммировать,
+      <div class="q-pa-sm">Если спаять пару датчиков и немного их запрограммировать,
         то можно записать показания этих датчиков в базу данных для анализа,
         ну или просто наблюдать и делать прогнозы</div>
       <div class="full-width flex flex-center q-pa-md">
@@ -50,7 +50,7 @@
           spinner-color="secondary"
         ></q-img>
       </div>
-      <div class="text-justify text-subtitle1 q-pa-sm">Вы приблизитесь к пониманию, если повторите этот проект наружной автономной Wi-Fi
+      <div class="q-pa-sm">Вы приблизитесь к пониманию, если повторите этот проект наружной автономной Wi-Fi
             метеостанции или реализуете задумку со своими датчиками.</div>
     </div>
   </q-page>
