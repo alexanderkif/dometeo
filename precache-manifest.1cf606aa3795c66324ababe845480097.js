f5efea67d6c3fb7fde6bed2061ee4b4d6c5e9a91
alexanderkif <alexander_kif@mail.ru>
2020-03-21 20:44:29 +0400

    Updates

---
+++ precache-manifest.1cf606aa3795c66324ababe845480097.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b897e836c1093bc6d8",
+    "url": "css/2.0756f9bd.css"
+  },
+  {
+    "revision": "22d5dc3787a95c894f6d",
+    "url": "css/app.66d806c5.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "cc057dc640093468380ce69f4b076ecc",
+    "url": "img/18650.cc057dc6.png"
+  },
+  {
+    "revision": "941633902dd59cc0125b0109b632088c",
+    "url": "img/18650holder.94163390.png"
+  },
+  {
+    "revision": "5e611c2a6e3a0360c8372982fb6f715c",
+    "url": "img/HowFront.5e611c2a.svg"
+  },
+  {
+    "revision": "975b633e1d477d01bdbc00f5e58bee10",
+    "url": "img/HowGetData.975b633e.svg"
+  },
+  {
+    "revision": "faf3b8d1874acdbcdf123c0bfa358518",
+    "url": "img/HowPOST.faf3b8d1.svg"
+  },
+  {
+    "revision": "5ae9ff4b027569b978f202f1cda7724a",
+    "url": "img/MeteoSteps.5ae9ff4b.svg"
+  },
+  {
+    "revision": "29c72afc7f5f452c46d6b0e2d33ac01b",
+    "url": "img/arduino.29c72afc.svg"
+  },
+  {
+    "revision": "e41a831c41d1de14854195ff8bbdf9c9",
+    "url": "img/backend.e41a831c.svg"
+  },
+  {
+    "revision": "c08dc6026210cced1d9dc5d3670b3b17",
+    "url": "img/bme280.c08dc602.png"
+  },
+  {
+    "revision": "b8b675aaf543a7429a8c93e696c72ef6",
+    "url": "img/desktop-tv.b8b675aa.png"
+  },
+  {
+    "revision": "2f2e24206103c96c2763b79833e033d0",
+    "url": "img/how.2f2e2420.svg"
+  },
+  {
+    "revision": "26920aa31a55e898f4f5e03fc99e3c64",
+    "url": "img/mobile-hor-menu-ip.26920aa3.png"
+  },
+  {
+    "revision": "bb95bd38a9593aed9ed59141d7428154",
+    "url": "img/mobile-vert-lg.bb95bd38.png"
+  },
+  {
+    "revision": "fcbad448530bda88e79b96bc01b10a45",
+    "url": "img/schema-min.fcbad448.png"
+  },
+  {
+    "revision": "427b8c36208313eba5e31693ed774cc2",
+    "url": "img/solar.427b8c36.png"
+  },
+  {
+    "revision": "fa82693dab09ca74e19b188ae08daf70",
+    "url": "img/wemos.fa82693d.png"
+  },
+  {
+    "revision": "a527dbdaa158a1fd5b99f3dce5da8b55",
+    "url": "index.html"
+  },
+  {
+    "revision": "18b897e836c1093bc6d8",
+    "url": "js/2.18b897e8.js"
+  },
+  {
+    "revision": "023a03d41cdf28f08e56",
+    "url": "js/3.023a03d4.js"
+  },
+  {
+    "revision": "b0328088d4fd76d7172b",
+    "url": "js/4.b0328088.js"
+  },
+  {
+    "revision": "91a647a69a7b9de4357e",
+    "url": "js/5.91a647a6.js"
+  },
+  {
+    "revision": "57bb26b22080c8334fb9",
+    "url": "js/6.57bb26b2.js"
+  },
+  {
+    "revision": "825b6942ace31485c734",
+    "url": "js/7.825b6942.js"
+  },
+  {
+    "revision": "0814736162602acb268e",
+    "url": "js/8.08147361.js"
+  },
+  {
+    "revision": "8370ccbedd801eca4970",
+    "url": "js/9.8370ccbe.js"
+  },
+  {
+    "revision": "22d5dc3787a95c894f6d",
+    "url": "js/app.00307efb.js"
+  },
+  {
+    "revision": "f29ffc8f002e200d19b2",
+    "url": "js/vendor.f29ffc8f.js"
+  },
+  {
+    "revision": "f550a4ee0e7cb37070e7982238859892",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "cc057dc640093468380ce69f4b076ecc",
+    "url": "statics/18650.png"
+  },
+  {
+    "revision": "941633902dd59cc0125b0109b632088c",
+    "url": "statics/18650holder.png"
+  },
+  {
+    "revision": "2f2e24206103c96c2763b79833e033d0",
+    "url": "statics/How.svg"
+  },
+  {
+    "revision": "5e611c2a6e3a0360c8372982fb6f715c",
+    "url": "statics/HowFront.svg"
+  },
+  {
+    "revision": "975b633e1d477d01bdbc00f5e58bee10",
+    "url": "statics/HowGetData.svg"
+  },
+  {
+    "revision": "faf3b8d1874acdbcdf123c0bfa358518",
+    "url": "statics/HowPOST.svg"
+  },
+  {
+    "revision": "5ae9ff4b027569b978f202f1cda7724a",
+    "url": "statics/MeteoSteps.svg"
+  },
+  {
+    "revision": "f0cf7b44112908b21ba582da94c7a496",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "29c72afc7f5f452c46d6b0e2d33ac01b",
+    "url": "statics/arduino.svg"
+  },
+  {
+    "revision": "e41a831c41d1de14854195ff8bbdf9c9",
+    "url": "statics/backend.svg"
+  },
+  {
+    "revision": "c08dc6026210cced1d9dc5d3670b3b17",
+    "url": "statics/bme280.png"
+  },
+  {
+    "revision": "b8b675aaf543a7429a8c93e696c72ef6",
+    "url": "statics/desktop-tv.png"
+  },
+  {
+    "revision": "a7c007e8831a0a2c41c1bf5fd36b9016",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "3e6153d4bd1160f853ba80694db2b75c",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "5372b018178c01af47d64d7a18a2c18e",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "9adbc6c5c035fc4bae0bf0c92e4444ea",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "6d5f45f8a9f48c00c522ac1ec4076f2a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "f6116383e2c8c3c5af9362a85663ae18",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "8bae517afa27b6ff6358fec66d383322",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "542f88fbde3f553a6b8cbfde0851d5e6",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "f0cf7b44112908b21ba582da94c7a496",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "2074fd46542ce3acfac663be62c60b10",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "8a3ef308df9469d65d4ae517505f47f3",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "45bd7387c1980646b7aaac6d84d84b39",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "fa1e55a88fd55c20c0b3b1a8a9120a8a",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "7203afdbc5e9e69a71aa91a401dffb30",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "520507fd80d2642570401143cd4b8bc1",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "26920aa31a55e898f4f5e03fc99e3c64",
+    "url": "statics/mobile-hor-menu-ip.png"
+  },
+  {
+    "revision": "bb95bd38a9593aed9ed59141d7428154",
+    "url": "statics/mobile-vert-lg.png"
+  },
+  {
+    "revision": "fcbad448530bda88e79b96bc01b10a45",
+    "url": "statics/schema-min.png"
+  },
+  {
+    "revision": "427b8c36208313eba5e31693ed774cc2",
+    "url": "statics/solar.png"
+  },
+  {
+    "revision": "fa82693dab09ca74e19b188ae08daf70",
+    "url": "statics/wemos.png"
+  }
+]);
